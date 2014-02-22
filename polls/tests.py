47d70f7edd6f126ea48e66fbb512c99745f32162
Ivo Flipse <ivoflipse@gmail.com>
2014-02-22 14:34:14 +0100

    Added a test case

--- polls/tests.py
+++ polls/tests.py
@@ -5,7 +5,12 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
+
+from polls.models import Poll
 
 
 class SimpleTest(TestCase):
@@ -14,3 +19,13 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class PollMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
